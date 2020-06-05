434ea9d5241c15ad92926d40c4f8a96b202eaaa3
Niko Lockenvitz <n.lockenvitz@online.de>
2020-06-05 15:40:41 +0200

    add pwa
    created with 'vue add pwa'

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
