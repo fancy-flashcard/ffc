5e9c5002a766fb11d8a5316f202036db28c83fbb
Rene Fischer <github@renefischer.org>
2020-06-20 01:17:47 +0200

    add info for pwa creation change themeColor
    Co-authored-by: Niko Lockenvitz <nl@nikolockenvitz.de>

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,13 @@ module.exports = {
   "publicPath": "/ffc/",
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  pwa: {
+    themeColor: "#363636",
+    msTileColor: "#3F51B5",
+    manifestOptions: {
+      name: "Fancy Flashcard",
+      short_name: "FFC",
+    }
+  }
 }
