fancy-flashcard/ffc

commit eef1d1d91bc5ba651815c5ac257a0a480a6a027f

fix #22: clear learning session on finish
add callback learning is finished which clears the learning session
-> prevents saving learning session after it was removed from local
storage (that lead to a situation where you continued a finished
learning session, see more details in #22)

src/components/learn/Learn.vue
@@ -91,6 +91,7 @@ export default class Learn extends LearnProps {
   }
 
   updateCurLearningElement() {
+    if (!this.learningSessionManager) return;
     this.curLearningElement = this.learningSessionManager.getCurrentLearningSessionElementWithCardDetails();
     this.updateRatingForCurrentLearningElement();
     saveLearningSessionManagerDataToLocalStorage(this.learningSessionManager);
@@ -181,7 +182,9 @@ export default class Learn extends LearnProps {
   }
 
   finishSession() {
-    finishLearningDialog(this, this.getBarsForFinishLearningDialog());
+    finishLearningDialog(this, this.getBarsForFinishLearningDialog(), () => {
+      this.learningSessionManager = null;
+    });
   }
 
   getBarsForFinishLearningDialog(): CustomDialogOptionsBarChartBar[] {

src/helpers/finishLearningDialogHelper.ts
@@ -9,7 +9,8 @@ import { clearLearningSessionManagerDataInLocalStorage } from "./learningSession
 
 export function finishLearningDialog(
   context: any,
-  bars: CustomDialogOptionsBarChartBar[]
+  bars: CustomDialogOptionsBarChartBar[],
+  callbackLearningFinished: { (): void }
 ) {
   const options = {
     title: "Finish Learning?",
@@ -32,6 +33,7 @@ export function finishLearningDialog(
             QuitLearningReason.NO_MORE_CARDS
           );
           clearLearningSessionManagerDataInLocalStorage();
+          callbackLearningFinished();
         },
       },
     ],

commit 46c715c652cf507cdb104e2faf5003af09838375

hotfix for bug introduced in eef1d1d

src/components/learn/Learn.vue
@@ -61,6 +61,7 @@ const LearnProps = Vue.extend({
 export default class Learn extends LearnProps {
   numberOfStarsInRating = 5;
   learningSessionManager = new LearningSessionManager([]);
+  isLearningSessionFinishedAndComponentWillBeDestroyedSoon = false;
   curLearningElement = {
     deckId: 0,
     cardId: 0,
@@ -78,6 +79,7 @@ export default class Learn extends LearnProps {
   };
 
   created() {
+    this.isLearningSessionFinishedAndComponentWillBeDestroyedSoon = false;
     this.$eventHub.$on(Event.SWIPE_LEFT_IN_LEARN, () => {
       this.moveToNext();
     });
@@ -91,7 +93,7 @@ export default class Learn extends LearnProps {
   }
 
   updateCurLearningElement() {
-    if (!this.learningSessionManager) return;
+    if (this.isLearningSessionFinishedAndComponentWillBeDestroyedSoon) return;
     this.curLearningElement = this.learningSessionManager.getCurrentLearningSessionElementWithCardDetails();
     this.updateRatingForCurrentLearningElement();
     saveLearningSessionManagerDataToLocalStorage(this.learningSessionManager);
@@ -183,7 +185,7 @@ export default class Learn extends LearnProps {
 
   finishSession() {
     finishLearningDialog(this, this.getBarsForFinishLearningDialog(), () => {
-      this.learningSessionManager = null;
+      this.isLearningSessionFinishedAndComponentWillBeDestroyedSoon = true;
     });
   }
 